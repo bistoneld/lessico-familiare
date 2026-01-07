d28236a85535f303a33ece224011db18a5968a63
LDrago <bistone@hotmail.it>
2026-01-07 02:32:54 +0100

    go

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Studio Lessico Familiare';
+export const SITE_DESCRIPTION = 'Benvenuti sul sito dello studio di psicoterapia "Lessico Familiare"';
